27edeab7fccb5b2d784fcec7299fff7d6e8fc2eb
SnehalPawar-1709 <b38_2022_ecse@yes.edu.in>
2026-06-29 15:31:30 +0530

    Add setupAdmin route to authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { loginAdmin, changePassword, verifyToken } = require('../controllers/authController');
+const { loginAdmin, changePassword, verifyToken, setupAdmin } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/login', loginAdmin);
 router.put('/change-password', protect, changePassword);
 router.get('/verify', protect, verifyToken);
+router.get('/setup-admin', setupAdmin);
 
 module.exports = router;
